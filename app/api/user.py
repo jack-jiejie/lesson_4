cf963e14cf02264f917e68dd5ee6e2d08f14deda
yin <jack@yindeMacBook-Pro.local>
2020-04-11 19:38:24 +0800

    flask学习4

--- app/api/user.py
+++ app/api/user.py
@@ -1,13 +1,14 @@
 __author__ = "yzj"
 __data__ = "2020/4/10 22:47"
 
-
-
 from flask import jsonify, Blueprint, request
 
 # 实例化蓝图对象
 # user = Blueprint('web', __name__)
 from . import api
+from ..forms.user import UserForm
+
+
 @api.route("/users")
 def users():
     result = {
@@ -17,15 +18,29 @@ def users():
     return jsonify(result)
 @api.route("/login")
 def login():
-    username = request.args["username"]
-    print(username)
-    result = {
-        "computer": 131314,
-        "brand": 1243141
-    }
-    return jsonify(result)
+    # 接收消息
+    # username = request.args["username"]
+    # print(username)
+    form = UserForm(request.args)
 
+    if form.validate():
+        username = form.username.data.strip()
+        print(username)
+        result = {
+            "status": 2001,
+            "message": "叫"
+        }
+        return jsonify(result)
+    else:
+        return jsonify(form.errors)
+    # return jsonify(result)
 
-
-
+@api.route("/getuserinfo", methods = ["GET", "POST"])
+def get_user_info():
+    if request.method == "GET":
+        form = UserForm(request.args)
+        print("get请求来了")
+    if request.method == "POST":
+        print("post请求来了")
+    return "这把有效了"
 
--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 __author__ = "yin"
 __data__ = "2020/4/9 13:06"
 
+from app1 import app
 
 DEBUG = True
-
+app.config["JSON_AS_ASCII"] = False
 
